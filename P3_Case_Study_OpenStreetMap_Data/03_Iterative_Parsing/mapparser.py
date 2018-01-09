ae9ad3dcba7c6469979fcd31bb5d8cfefd8e2de3
adamofatom <yemengtao1986@hotmail.com>
2018-01-09 23:10:14 +0800

    Project 1

--- P3_Case_Study_OpenStreetMap_Data/03_Iterative_Parsing/mapparser.py
+++ P3_Case_Study_OpenStreetMap_Data/03_Iterative_Parsing/mapparser.py
@@ -30,13 +30,13 @@ def test():
     tags = count_tags('example.osm')
     pprint.pprint(tags)
     assert tags == {'bounds': 1,
-                     'member': 3,
-                     'nd': 4,
-                     'node': 20,
-                     'osm': 1,
-                     'relation': 1,
-                     'tag': 7,
-                     'way': 1}
+                    'member': 3,
+                    'nd': 4,
+                    'node': 20,
+                    'osm': 1,
+                    'relation': 1,
+                    'tag': 7,
+                    'way': 1}
 
     
 
