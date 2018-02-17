2f555c24a77c3b627fd05dcf4bcad9924774850a
adamofatom <yemengtao1986@hotmail.com>
2018-02-17 22:48:08 +0800

    modify it

--- P7_Machine_Learning/01_Naive_Bayes/ClassifyNB.py
+++ P7_Machine_Learning/01_Naive_Bayes/ClassifyNB.py
@@ -1,4 +1,4 @@
-def classify(features_train, labels_train):   
+def classify(features_train, labels_train):
     # import the sklearn module for GaussianNB
     # create classifier
     # fit the classifier on the training features and labels
